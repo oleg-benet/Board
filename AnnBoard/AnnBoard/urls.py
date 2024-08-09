9ee9746c376a5f1fc68cf53576f75ceff111196b
Oleg <benetal@yandex.ru>
2024-08-09 23:22:08 +0700

    set ckeditor

--- AnnBoard/AnnBoard/urls.py
+++ AnnBoard/AnnBoard/urls.py
@@ -14,14 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from ckeditor_uploader.views import upload, browse
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.contrib.auth.decorators import login_required
+from django.urls import path, include, re_path
+from django.views.decorators.cache import never_cache
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('', include('announcement.urls')),
     path('accounts/', include('allauth.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls'))
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^upload/', login_required(upload), name='ckeditor_upload'),
+    re_path(r'^browse/', login_required(never_cache(browse)), name='ckeditor_browse')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
